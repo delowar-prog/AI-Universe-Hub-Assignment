6b0149c972e143aa18942e743687ac966bd34ff1
Delowar Hossain <bscdelowar@yahoo.com>
2023-03-03 01:51:22 +0600

    show data into the card

--- js/js.js
+++ js/js.js
@@ -6,6 +6,26 @@ const loadAiTools=async()=>{
 }
 
 const displayAiTools=data=>{
-    
+    const aiContainer=document.getElementById('ai-container');
+    data.slice(0,6).forEach(singleAi => {
+        const {image, features}=singleAi;
+        aiContainer.innerHTML += `
+        <div class="col">
+            <div class="card p-3 w-100">
+                <img src="${image}" style="height:250px" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">Features</h5>
+                <ol id="feature-list">
+                   <li>${features[0]}</li>
+                   <li>${features[1]}</li>
+                   <li>${features[2]?features[2]:''}</li>
+                </ol>
+               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        `;
+        
+    });
 }
 loadAiTools();
