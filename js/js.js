ccd5b325ad47b3c1d45bab7550d50d19a7bbb6be
Delowar Hossain <bscdelowar@yahoo.com>
2023-03-03 12:37:58 +0600

    show all items when clicked

--- js/js.js
+++ js/js.js
@@ -1,31 +1,58 @@
-const loadAiTools=async()=>{
+const loadAiTools=async(limit)=>{
     const url=`https://openapi.programming-hero.com/api/ai/tools`;
     const res=await fetch(url);
     const data=await res.json();
-    displayAiTools(data.data.tools);
+    displayAiTools(data.data.tools, limit);
 }
 
-const displayAiTools=data=>{
+const displayAiTools=(data,limit)=>{
     const aiContainer=document.getElementById('ai-container');
-    data.slice(0,6).forEach(singleAi => {
-        const {image, features}=singleAi;
-        aiContainer.innerHTML += `
+    aiContainer.innerText='';
+    // Display Show all btn
+    if(data.length > limit){
+        data=data.slice(0, limit);
+         document.getElementById('btn-showAll').classList.remove('d-none');
+     }else{
+         document.getElementById('btn-showAll').classList.add('d-none');
+     }
+     //Iterate all Items
+    data.forEach(singleAi => {
+        console.log(singleAi)
+        const {image, features, name, published_in}=singleAi;
+        aiContainer.innerHTML +=`
         <div class="col">
             <div class="card p-3 w-100">
                 <img src="${image}" style="height:250px" class="card-img-top" alt="...">
                 <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                <ol id="feature-list">
-                   <li>${features[0]}</li>
-                   <li>${features[1]}</li>
-                   <li>${features[2]?features[2]:''}</li>
-                </ol>
-               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <ol>
+                    <li>${features[0]}</li>
+                    <li>${features[1]}</li>
+                    <li class="${features[2]===undefined?'d-none':''}">${features[2]}</li>
+                    <li class="${features[3]===undefined?'d-none':''}">${features[3]}</li>
+                    <li class="${features[4]===undefined?'d-none':''}">${features[4]}</li>
+                    <li class="${features[5]===undefined?'d-none':''}">${features[5]}</li>
+               </ol>
+               <hr/>
+                    <div class="d-flex justify-content-between align-items-center mt-4">
+                        <div>
+                            <h3 class="mb-3">${name}</h3>
+                            <p class="fs-5"><i class="fa-solid fa-calendar-days fs-4"></i> ${published_in}</p>
+                        </div>
+                        <button style="border-radius:50%; padding:10px 15px;" class="fs-5 border-0 text-danger"><i class="fa-solid fa-arrow-right"></i></button>
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-        
+        </div>`;
     });
 }
-loadAiTools();
+
+//show default 6 Items
+loadAiTools(6);
+
+
+
+/*
+${features.map(element =>`
+<li>${element}</li>
+`)}*/
